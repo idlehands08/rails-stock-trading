421573dbc88a0428239f0693d1dc1a7a127688e3
idlehands08 <geofftd08@gmail.com>
2022-01-12 12:59:48 +0800

    user-model rspec test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'When creating users, ' do
+    it 'email must not be null' do
+      user = User.new(email: nil, encrypted_password: '123123', first_name: 'test', last_name: 'test')
+      expect(user).not_to be_valid
+    end
+    
+    it 'password must not be null' do
+      user = User.new(email: 'rspectest@example.com', encrypted_password: nil, first_name: 'test', last_name: 'test')
+      expect(user).not_to be_valid
+    end
+    
+    it 'first name must not be null' do
+      user = User.new(email: 'rspectest@example.com', encrypted_password: '123123', first_name: nil, last_name: 'test')
+      expect(user).not_to be_valid
+    end
+    
+    it 'last name must not be null' do
+      user = User.new(email: 'rspectest@example.com', encrypted_password: '123123', first_name: 'test', last_name: nil)
+      expect(user).not_to be_valid
+    end
+  end
+end
