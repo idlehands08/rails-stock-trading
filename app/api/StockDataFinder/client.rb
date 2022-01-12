bc73c9e149e880533d52de7a46126e49ecccb43c
idlehands08 <geofftd08@gmail.com>
2022-01-12 11:51:22 +0800

    Stock data api on footer

--- app/api/StockDataFinder/client.rb
+++ app/api/StockDataFinder/client.rb
@@ -3,8 +3,8 @@ require 'json'
 module StockDataFinder
   class Client
     # symbols='AAPL,ADBE,AMD,AMZN,CRM,GOOG,FB,JPM,TSLA,MA,MSFT,NEE,NVDA,SBUX,UNH,V,WMT'
-    #  SYMBOLS = 'AAPL,AMZN,GOOG,MSFT,NVDA,TSLA,MA,JPM,WMT,V,NEE,UNH,CRM,ADBE,AMD,FB,SBUX,SQ,HD,MCD,PYPL,BA,NFLX,NOW'
-     SYMBOLS = 'NEE,UNH,CRM'
+    # SYMBOLS = 'AAPL,AMZN,GOOG,MSFT,NVDA,TSLA,MA,JPM,WMT,V,NEE,UNH,CRM,ADBE,AMD,FB,SBUX,SQ,HD,MCD,PYPL,BA,NFLX,NOW'
+     SYMBOLS = 'TSLA,AAPL,GOOG'
       def self.price_update
             response = Request.call(http_method: 'get', endpoint:"/v1/data/quote?symbols=#{SYMBOLS}")
             if !response.nil?
@@ -19,8 +19,15 @@ module StockDataFinder
                 end
             end
         end
-        def self.api_check
+        def self.display_stocks
             response = Request.call(http_method: 'get', endpoint:"/v1/data/quote?symbols=#{SYMBOLS}")
+            if !response.nil?
+                response[:data]["data"].map do |stock_info|
+                    p "#{stock_info['ticker']} - #{stock_info['price']}USD"
+                end
+            else
+                p 'Error connectiong to StockData.org'
+            end
         end
     end
 end
