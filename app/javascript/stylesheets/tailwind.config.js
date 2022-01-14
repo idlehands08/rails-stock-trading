ba8108e7242d5108b892b6c2c9dc33d46f8add69
idlehands08 <geofftd08@gmail.com>
2022-01-14 14:13:53 +0800

    removed tailwind colors settings

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -8,14 +8,6 @@ module.exports = {
 darkMode: false, // or 'media' or 'class'
 theme: {
   extend: {
-    colors: {
-      'stock-gold': '#F1A208',
-      'stock-teal': '#558599',
-      'stock-black': '#2D2D2A',
-      'stock-green': '#488B49',
-      'light-blue': colors.lightblue,
-      'cyan': colors.cyan,
-    },
   },
 },
 variants: {
